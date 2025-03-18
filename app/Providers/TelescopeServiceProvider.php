8d033171c6b7ca6fc3b1de3df82bb2f3ebfd2c5d
Bkoura-eng <youssefsalem383@gmail.com>
2025-03-18 04:31:40 +0200

    18-3-2025

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -57,9 +57,8 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                'taylor@laravel.com',
+                //
             ]);
         });
     }
-    
 }
