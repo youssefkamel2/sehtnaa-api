834da86b060d7bf3247aec4165881776cb8de8ee
Bkoura-eng <youssefsalem383@gmail.com>
2025-03-18 02:41:52 +0200

    test job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,6 +14,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            Log::info('Scheduler is running!');
+        })->everyMinute();
         // clear activity logs after 48 hours
         $schedule->command('activitylog:clean')->daily();
         // clear laravel telescope after 48 hours
