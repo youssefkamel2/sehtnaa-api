9025f10db2fd19d22a821064077585d7de4602d7
Bkoura-eng <youssefsalem383@gmail.com>
2025-04-17 22:19:01 +0200

    notifications

--- app/Models/NotificationLog.php
+++ app/Models/NotificationLog.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NotificationLog extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'campaign_id',
+        'user_id',
+        'title',
+        'body',
+        'data',
+        'is_sent',
+        'error_message'
+    ];
+
+    protected $casts = [
+        'data' => 'array',
+        'is_sent' => 'boolean'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
