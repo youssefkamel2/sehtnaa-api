8416c8c11b2c74013eec9b76a76a69a4b1760244
Bkoura-eng <youssefsalem383@gmail.com>
2025-05-07 21:58:51 +0300

    accept request

--- app/Services/ProviderNotifier.php
+++ app/Services/ProviderNotifier.php
@@ -296,7 +296,7 @@ class ProviderNotifier
             
             $data = [
                 'request_id' => (string) $request->id,
-                'customer_name' => $request->name ?? 'Unknown Customer',
+                'customer_name' => $request->first_name . $request->last_name ?? 'Unknown Customer',
                 'service_names' => $serviceNames,
                 'services_count' => $request->services->count(),
                 'total_price' => $request->total_price,
